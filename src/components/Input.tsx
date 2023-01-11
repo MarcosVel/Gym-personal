67598c329a42440d2fe4837586e0b505ff548b37
MarcosVel <marcos.veloso@sempreceub.com>
2023-01-11 15:51:29 -0300

    feat: focus on input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,12 +7,16 @@ export default function Input({ ...rest }: IInputProps) {
       bg="gray.700"
       h={14}
       px={4}
-      borderWidth={0}
+      borderColor="gray.700"
       fontSize="md"
       color="gray.100"
       fontFamily="body"
       mb={4}
       placeholderTextColor="gray.300"
+      _focus={{
+        bg: "gray.700",
+        borderColor: "green.500",
+      }}
       {...rest}
     />
   );
