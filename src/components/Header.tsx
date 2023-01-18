0629a83c48c70e2cb01b3b5233531b2508766df0
MarcosVel <marcos.veloso@sempreceub.com>
2023-01-18 19:36:20 -0300

    feat: user photo component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import { Heading, HStack, Text, VStack } from "native-base";
 import React from "react";
+import UserPhoto from "./UserPhoto";
 
 export default function Header() {
   return (
     <HStack bg="gray.600" pt={12} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/MarcosVel.png" }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
