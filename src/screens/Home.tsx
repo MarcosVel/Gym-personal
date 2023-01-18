15e36dd69dd421f26854fb0d7b205273f54c0c7a
MarcosVel <marcos.veloso@sempreceub.com>
2023-01-18 20:15:40 -0300

    feat: creating group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,13 @@
 import { VStack } from "native-base";
+import Group from "../components/Group";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <VStack flex={1}>
       <Header />
+
+      <Group name="Grupo" />
     </VStack>
   );
 }
