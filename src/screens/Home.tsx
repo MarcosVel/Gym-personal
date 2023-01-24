04f9b0281c8d972b37fa44ac513c05f3c5aec285
MarcosVel <marcos.veloso@sempreceub.com>
2023-01-23 23:42:09 -0300

    feat: header exercise list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { FlatList, HStack, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 import { useState } from "react";
 import Group from "../components/Group";
 import Header from "../components/Header";
@@ -32,6 +32,17 @@ export default function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
