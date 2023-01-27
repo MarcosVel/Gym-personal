5af7531365d1e888dd8a50c8c2ac596979d25382
MarcosVel <marcos.veloso@sempreceub.com>
2023-01-27 19:51:25 -0300

    feat: history card component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,12 @@
 import { VStack } from "native-base";
+import HistoryCard from "../components/HistoryCard";
 import ScreenHeader from "../components/ScreenHeader";
 
 export default function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercício" />
+      <HistoryCard />
     </VStack>
   );
 }
