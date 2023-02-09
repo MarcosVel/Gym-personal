d744c65823534cbf2340672d16c3df1d57815152
MarcosVel <marcos.veloso@sempreceub.com>
2023-02-08 22:02:57 -0300

    feat: header and user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,25 @@
-import { Center, Text } from "native-base";
+import { Center, ScrollView, VStack } from "native-base";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ScreenHeader from "../components/ScreenHeader";
+import UserPhoto from "../components/UserPhoto";
 
 export default function Profile() {
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <SafeAreaView style={{ flex: 1 }}>
+      <VStack flex={1}>
+        <ScreenHeader title="Perfil" />
+
+        <ScrollView>
+          <Center mt={6} px={10}>
+            <UserPhoto
+              source={{ uri: "https://github.com/MarcosVel.png" }}
+              alt="User photo"
+              size={33}
+              height={33}
+            />
+          </Center>
+        </ScrollView>
+      </VStack>
+    </SafeAreaView>
   );
 }
