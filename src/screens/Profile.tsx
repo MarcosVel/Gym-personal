21af9e9ec47bfbebe7870be37d2d51c0efa8faa4
MarcosVel <marcos.veloso@sempreceub.com>
2023-02-08 22:28:20 -0300

    feat: add skeleton for photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,14 @@
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ScreenHeader from "../components/ScreenHeader";
 import UserPhoto from "../components/UserPhoto";
 
+const PHOTO_SIZE = 33;
+
 export default function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <VStack flex={1}>
@@ -11,12 +16,21 @@ export default function Profile() {
 
         <ScrollView>
           <Center mt={6} px={10}>
-            <UserPhoto
-              source={{ uri: "https://github.com/MarcosVel.png" }}
-              alt="User photo"
-              size={33}
-              height={33}
-            />
+            {photoIsLoading ? (
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.400"
+                endColor="gray.600"
+              />
+            ) : (
+              <UserPhoto
+                source={{ uri: "https://github.com/MarcosVel.png" }}
+                alt="User photo"
+                size={PHOTO_SIZE}
+              />
+            )}
           </Center>
         </ScrollView>
       </VStack>
