407aa63f6cee61eaac000509b5233324cc3d5a79
MarcosVel <marcos.veloso@sempreceub.com>
2023-02-12 22:18:38 -0300

    feat: navigation to exercise deatils screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,26 @@
-import { Center, Text } from "native-base";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { Icon, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AppNavigatorRoutesProps } from "../routes/app.routes";
 
 export default function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <SafeAreaView style={{ flex: 1 }}>
+      <VStack flex={1}>
+        <VStack px={8} bg="gray.600" pt={4}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+          </TouchableOpacity>
+        </VStack>
+      </VStack>
+    </SafeAreaView>
   );
 }
