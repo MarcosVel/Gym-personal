83d40377a81a5db4c42b1c634934555069f7a060
MarcosVel <marcos.veloso@sempreceub.com>
2023-03-13 23:53:40 -0300

    feat: add data of history cards

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -45,7 +45,7 @@ export default function History() {
         <SectionList
           sections={exercises}
           keyExtractor={item => item.id}
-          renderItem={({ item }) => <HistoryCard />}
+          renderItem={({ item }) => <HistoryCard data={item} />}
           renderSectionHeader={({ section }) => (
             <Heading
               color="gray.200"
